8762fb005d6dffb0fbe761dd5c49e833a98d4fdd
NoumanFatta <81779387+NoumanFatta@users.noreply.github.com>
2021-07-26 21:59:32 +0500

    Add files via upload

--- script2.js
+++ script2.js
@@ -8,9 +8,11 @@ for (var i = 0; i < localStorage.length; i++) {
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
-    cell1.innerHTML = retrievedObject.name;
-    cell2.innerHTML = retrievedObject.gender;
-    cell3.innerHTML = retrievedObject.address;
-    cell4.innerHTML = retrievedObject.education;
-    cell5.innerHTML = retrievedObject.profession;
+    var cell6 = row.insertCell(5);
+    cell1.innerHTML = (i+1);
+    cell2.innerHTML = retrievedObject.name;
+    cell3.innerHTML = retrievedObject.gender;
+    cell4.innerHTML = retrievedObject.address;
+    cell5.innerHTML = retrievedObject.education;
+    cell6.innerHTML = retrievedObject.profession;
 }
